9e0ce201bc492fb5575cd13e58a5635c35fa23c9
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-05-01 08:56:14 +0300

    first commit

---
+++ Assessment.js
@@ -0,0 +1,51 @@
+//Create a function called getAvailableBooks that returns an array of all available
+//books.
+function getAvailableBooks(){
+    let EmptyArray=[];
+    var books =("The Catcher in the Rye","Great Expectation","Crime and Punishment","The Great Gatsby","The Odyssey");
+    return(Book);
+    
+}
+console.log(books);
+
+//Create a function getBooksByAuthor that takes an author's name as an argument and
+//returns an array of all books by that author.
+function getBooksByAuthor(){
+    let EmptyArray=[];
+    var name=("J.D Salinger","Charles Dickens","F. Scott Fitzgerald","Homer");
+    return(books);
+}
+console.log(name);
+
+//Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+   // publicationYear, and isAvailable).
+   function addNewBook(){
+    let EmptyArray=[];
+    var addNewBook=("title","author","publicationYear");
+
+    
+}
+console.log(addNewBook);
+
+
+//Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+function checkoutBook(){
+    var title=("The Catcher in the Rye");
+    return(title);
+}
+console.log(titles);
+
+
+//Create a function returnBook that takes a book title as an argument and changes the
+//book's isAvailable property to true. If the book is not found in the library, the function
+//should return a message indicating that the book does not belong to the library.
+function returnBook(){
+    let EmptyArray=[];
+    var title=("The Catcher in the Rye");
+    return(title);
+}
+console.log(titles);
+   
